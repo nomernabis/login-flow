16e9cb59d9ed9b55022ea111fca299eac4f63d9e
pavel_cpp <pasha.dunin.94@gmail.com>
2019-01-28 02:43:49 +0600

    icons added

--- src/components/table/TableItem.js
+++ src/components/table/TableItem.js
@@ -1,13 +1,17 @@
 import React from "react"
 import "../../styles/Table.css"
-import { showInfoModal } from "../../actions";
+import { showInfoModal } from "../../actions"
+import EditIcon from "../../icons/edit.svg"
+import TrashIcon from "../../icons/delete.svg"
 
 export const TableItem = ({ item, onEdit, onDelete }) => {
     const content = Object.keys(item).map(prop => (<div className="table-col">{ item[prop] }</div>))
     return(
         <div className="table-row material-text-normal">
             {content}
-            <div className="table-col"><button onClick={onEdit}>Edit</button><button onClick={onDelete}>Delete</button></div>
+            <div className="table-col">
+                <button className="btn" onClick={onEdit}><EditIcon fill="#fff" width={24} height={24} /></button>
+                <button className="btn" onClick={onDelete}><TrashIcon fill="#fff" width={24} height={24} /></button></div>
         </div>
     )
 }
